2d10ba6d657425613b4292d6ae9d0e088ef2f5b8
Jorge Ossa <jorgehossa@gmail.com>
2022-01-15 22:43:24 -0500

    📦 NEW: Add first actions

--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 interface ButtonProps {
   text: string;
   color?: 'default' | 'primary' | 'secondary';
   size?: 'default' | 'large';
+  action: (number: string) => void;
 }
 
 const CalcButton = ({
   text,
   color = 'default',
   size = 'default',
+  action,
 }: ButtonProps) => {
   return (
-    <View
-      style={[
-        styles.button,
-        color !== 'default'
-          ? color === 'primary'
-            ? styles.buttonColorPrimary
-            : styles.buttonColorSecondary
-          : styles.buttonColorDefault,
-        size === 'large' ? styles.buttonSizeLarge : null,
-      ]}>
-      <Text
+    <TouchableOpacity onPress={() => action(text)}>
+      <View
         style={[
-          styles.buttonText,
-          color === 'primary'
-            ? styles.buttonTextBlack
-            : styles.buttonTextDefault,
+          styles.button,
+          color !== 'default'
+            ? color === 'primary'
+              ? styles.buttonColorPrimary
+              : styles.buttonColorSecondary
+            : styles.buttonColorDefault,
+          size === 'large' ? styles.buttonSizeLarge : null,
         ]}>
-        {text}
-      </Text>
-    </View>
+        <Text
+          style={[
+            styles.buttonText,
+            color === 'primary'
+              ? styles.buttonTextBlack
+              : styles.buttonTextDefault,
+          ]}>
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {theme} from '../theme/Default';
 import CalcButton from '../components/CalcButton';
 
 const CalculatorScreen = () => {
+  const [result, setResult] = useState('0');
+  const [operation, setOperation] = useState('0');
+
+  const clean = () => {
+    setResult('0');
+    setOperation('0');
+  };
+
+  const createOperation = (operationNumber: string) => {
+    setOperation(operation + operationNumber);
+  };
+
   return (
-    <View style={styles.constainer}>
-      <Text style={styles.operationText}>1,500.00</Text>
-      <Text style={styles.resultText}>1,500.00</Text>
+    <View style={styles.container}>
+      <Text style={styles.operationText}>{operation}</Text>
+      <Text style={styles.resultText}>{result}</Text>
       <View style={theme.row}>
-        <CalcButton text="C" color="primary" />
-        <CalcButton text="+/-" color="primary" />
-        <CalcButton text="del" color="primary" />
-        <CalcButton text="÷" color="secondary" />
+        <CalcButton text="C" color="primary" action={clean} />
+        <CalcButton text="+/-" color="primary" action={null} />
+        <CalcButton text="del" color="primary" action={null} />
+        <CalcButton text="÷" color="secondary" action={null} />
       </View>
       <View style={theme.row}>
-        <CalcButton text="7" />
-        <CalcButton text="8" />
-        <CalcButton text="9" />
-        <CalcButton text="×" color="secondary" />
+        <CalcButton text="7" action={createOperation} />
+        <CalcButton text="8" action={createOperation} />
+        <CalcButton text="9" action={createOperation} />
+        <CalcButton text="×" color="secondary" action={null} />
       </View>
       <View style={theme.row}>
-        <CalcButton text="4" />
-        <CalcButton text="5" />
-        <CalcButton text="6" />
-        <CalcButton text="−" color="secondary" />
+        <CalcButton text="4" action={createOperation} />
+        <CalcButton text="5" action={createOperation} />
+        <CalcButton text="6" action={createOperation} />
+        <CalcButton text="−" color="secondary" action={null} />
       </View>
       <View style={theme.row}>
-        <CalcButton text="1" />
-        <CalcButton text="2" />
-        <CalcButton text="3" />
-        <CalcButton text="+" color="secondary" />
+        <CalcButton text="1" action={createOperation} />
+        <CalcButton text="2" action={createOperation} />
+        <CalcButton text="3" action={createOperation} />
+        <CalcButton text="+" color="secondary" action={null} />
       </View>
       <View style={theme.row}>
-        <CalcButton text="0" size="large" />
-        <CalcButton text="." />
-        <CalcButton text="=" color="secondary" />
+        <CalcButton text="0" action={createOperation} size="large" />
+        <CalcButton text="." action={createOperation} />
+        <CalcButton text="=" color="secondary" action={null} />
       </View>
     </View>
   );
@@ -44,7 +56,7 @@ const CalculatorScreen = () => {
 export default CalculatorScreen;
 
 const styles = StyleSheet.create({
-  constainer: {
+  container: {
     flex: 1,
     justifyContent: 'flex-end',
     paddingHorizontal: 20,
